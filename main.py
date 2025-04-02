72dc981f4d57d2a1974209efe4cf44f2b9bc2847
Amir Dehkordi <amir.dehkordi98@gmail.com>
2025-04-02 17:16:30 +0100

    Data Retrieval

---
+++ main.py
@@ -0,0 +1,65 @@
+import spacy
+import os
+import re
+import asyncio
+import nest_asyncio
+import pandas as pd
+import time
+from ib_insync import *
+import math
+import datetime
+import concurrent.futures
+# import config as c
+
+
+IB_IP, IB_PT, CL_ID = '127.0.0.1', 4002, 42
+risk = 0.001
+max_margin_allowed = 0.9
+trade_close_time = '15:55'
+
+date_today = datetime.datetime.today().strftime('%Y-%m-%d')
+print('====================')
+print(f'Date: {date_today}')
+print('====================')
+
+print('========== Connecting to IB ==========')
+ib = IB()
+print(f"Connecting to IB at {IB_IP}:{IB_PT} with Client ID={CL_ID}...")
+try:
+    ib.connect(IB_IP, IB_PT, clientId=CL_ID)
+except Exception as e:
+    raise RuntimeError(f"Failed to connect to IB: {e}")
+
+if not ib.isConnected():
+    raise RuntimeError("Connection to IB could not be established.")
+print("Successfully connected to IB.")
+print()
+
+
+contract = Stock('AAPL', 'SMART', 'USD')
+ib.qualifyContracts(contract)
+
+# Real-time market data (streaming)
+ib.reqMktData(contract)
+
+# Historical data
+bars = ib.reqHistoricalData(
+    contract,
+    endDateTime='',
+    durationStr='1 D',
+    barSizeSetting='1 min',
+    whatToShow='TRADES',
+    useRTH=True
+)
+
+# Convert to a pandas DataFrame
+df = util.df(bars)
+
+print(df.head())  # Print the first few rows
+print(df.tail())  # Print the last few rows
+
+# print(df.to_markdown())
+
+
+# Disconnect
+ib.disconnect()
